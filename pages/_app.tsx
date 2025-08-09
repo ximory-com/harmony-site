1cf11650142b6468e08945923964f9c5c0b4deec
ximory-com <app@ximory.com>
2025-08-09 08:38:21 +0800

    更新 _app.tsx
    修复发布错误

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 import Layout from '../components/Layout'
 export default function App({ Component, pageProps }: AppProps){
   return (<Layout><Component {...pageProps} /></Layout>)
